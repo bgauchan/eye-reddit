b54d69b9553e409f018f53067fd342d4684594ce
BarDan <Zoro@Bardans-Work-MBP.local>
2020-04-01 02:25:40 -0400

    post has all the info on screen now

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,22 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledListItem = styled.li`
+    border-top: 1px solid #f1f1f1;
     display: flex;
     margin: 0.8rem 0;
+    padding: 15px 15px 2px;
+
+    &:first-child {
+        border-top: 0;
+        padding-top: 5px;
+    }
 `
 
 const StyledLink = styled.a`
     display: block;
-    line-height: 1.75rem;
-    padding-left: 1rem;
+    font-size: 1.05rem;
+    letter-spacing: -0.01rem;
+    line-height: 1.35rem;
+    margin-bottom: 0.45rem;
     text-decoration: none;
 
     :visited {
-        color: #adadad;
-    }
-
-    :hover {
-        text-decoration: underline;
+        color: #8e8d8d;
     }
 `
 
@@ -32,18 +37,109 @@ const StyledScoreDiv = styled.div`
     height: 1.8rem;
 `
 
+const StyledInfoDiv = styled.div`
+    font-size: 0.8rem;
+    padding-left: 1rem;
+
+    .submitted_info {
+        color: grey;
+        display: inline-block;
+        font-size: 0.7rem;
+        font-style: italic;
+        margin-left: 0.5rem;
+    }
+`
+const StyledGenericLink = styled.a`
+    color: orchid;
+    text-decoration: none;
+
+    :hover {
+        text-decoration: underline;
+    }
+`
+const StyledUserProfileLink = styled(StyledGenericLink)`
+    color: #0720ab;
+    margin-left: 0.2rem;
+`
+const StyledSubredditLink = styled(StyledGenericLink)`
+    color: grey;
+    font-size: 0.7rem;
+    margin-left: 0.4rem;
+`
+
+const timeDifference = (postCreatedTime) => {
+    let diff = Date.now() - new Date(postCreatedTime * 1000)
+    var daysDifference = Math.floor(diff / 1000 / 60 / 60 / 24)
+    diff -= daysDifference * 1000 * 60 * 60 * 24
+
+    let hoursDifference = Math.floor(diff/1000/60/60)
+    diff -= hoursDifference * 1000 * 60 * 60
+
+    let minutesDifference = Math.floor(diff / 1000 / 60)
+    diff -= minutesDifference * 1000 * 60
+
+    let secondsDifference = Math.floor(diff/1000);
+
+    if(daysDifference === 1) {
+        return daysDifference + ' day ago'
+    } 
+    
+    if(daysDifference > 0) {
+        return daysDifference + ' days ago'
+    }
+
+    if(daysDifference < 1) {
+        if(hoursDifference === 1) {
+            return hoursDifference + ' hour ago'
+        } else {
+            return hoursDifference + ' hours ago'
+        }
+    }
+
+    if(hoursDifference < 1) {
+        if(minutesDifference === 1) {
+            return minutesDifference + ' min ago'
+        } else {
+            return minutesDifference + ' mins ago'
+        }
+    }
+
+    if(minutesDifference < 60) {        
+        return secondsDifference + ' secs ago'
+    }
+
+    return ''
+}
+
 const Post = (props) => {
     let { post } = props
-    let url = 'https://old.reddit.com/' + post.permalink
+    let domain = 'https://old.reddit.com/'
+    let url = domain + post.permalink
+    let submittedTimeAgo = timeDifference(post.created_utc)
+    let userProfileUrl = domain + post.author
+    let subredditUrl = domain + post.subreddit_name_prefixed
 
     return (
-        <StyledListItem>
+        <StyledListItem data-post-id={post.id} >
             <StyledScoreDiv>
                 { post.score }
             </StyledScoreDiv>
-            <StyledLink href={url} target='_blank'>
-                { post.title }
-            </StyledLink>
+            <StyledInfoDiv>
+                <StyledLink href={url} target='_blank'>
+                    { post.title }
+                </StyledLink>
+                <StyledGenericLink href={url} target='_blank'>
+                    { post.num_comments } comments
+                </StyledGenericLink>
+                <div className='submitted_info'>
+                    Submitted { submittedTimeAgo } by 
+                    <StyledUserProfileLink href={userProfileUrl} target='_blank'>{ post.author } </StyledUserProfileLink>
+                    on
+                </div>
+                <StyledSubredditLink className='subreddit_name' href={subredditUrl} target='_blank'>
+                    { post.subreddit_name_prefixed }
+                </StyledSubredditLink>
+            </StyledInfoDiv>
         </StyledListItem>
     )
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Post from './Post'
+import styled from 'styled-components'
+
+const StyledList = styled.ul`
+    border: 1px solid #f1f1f1;
+    border-radius: 4px;
+`
 
 const Posts = (props) => {
     let { subreddit } = props
 
     return (
-        <ul>
+        <StyledList>
             { subreddit.items && subreddit.items.map(item => (
                 <Post key={item.id} post={item} />
             ))}
-        </ul>
+        </StyledList>
     )
 }
 
